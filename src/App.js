06a0a7f578c4b8e315fe3c05acf287415ee89775
dolgovdv <46597308+dolgovdv@users.noreply.github.com>
2020-04-29 22:15:21 +0300

    fix: App
    work App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import "./styles.css";
-import "./styles/App.scss";
+import React from "react"
+import "./styles.css"
+import "./styles/App.scss"
 
-import { Router } from "react-router-dom";
-import Routes from "./Routes";
+import { Router } from "react-router-dom"
+import Routes from "./Routes"
+
+import { createBrowserHistory } from "history"
+const browserHistory = createBrowserHistory()
 
 export default function App() {
   return (
     <div className="App">
-      <Router>
+      <Router history={browserHistory}>
         <Routes />
       </Router>
     </div>
-  );
+  )
 }
